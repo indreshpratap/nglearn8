74b14ea5cee689c92c5d69057347bfe2197d42e6
Indresh <ips.indresh@gmail.com>
2018-05-13 16:10:40 +0530

    event and for loop

--- src/app/components/property-binding.component.ts
+++ src/app/components/property-binding.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 
+
 @Component({
 selector:"app-property-binding",
 templateUrl:'./property-binding.component.html',
@@ -8,6 +9,30 @@ styles:['.rowgen001 { border:1px solid; padding:5px}',
 })
 export class PropertyBindingComponent {
     genid = "rowgen001";
-
     description = "This is a property binding component";
+
+    colors;
+
+    constructor(){
+        this.colors = ['White','Green','Yellow'];
+    }
+
+    doSave(){
+     alert("Data saved");
+     this.genid = "rowgen002";
+    }
+
+    doClickEvent(event){
+        console.log(event);
+        
+    }
+
+    onEnter(event,info){
+        console.log(info, event);
+    }
+
+    addColor(input){
+        console.log(input.value);
+        this.colors.push(input.value);
+    }
 }
