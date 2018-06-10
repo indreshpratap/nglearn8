5eb48a90492c45891327939d01572f8a3069c868
Indresh <ips.indresh@gmail.com>
2018-06-10 16:44:30 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { PropertyBindingComponent } from './components/property-binding.component';
 import { ProductListingComponent } from './components/products/product-listing.component';
 import { ProductCardComponent } from './components/products/product-card.component';
+import { AboutUsComponent } from './components/aboutus.component';
 
+const routes: Routes = [
+  {path:"",redirectTo:"products",pathMatch:"full" },
+  { path: "property-binding", component: PropertyBindingComponent },
+  { path: "products", component: ProductListingComponent },
+  {path:"aboutus",component:AboutUsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent, HeaderComponent, PropertyBindingComponent,
     ProductListingComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -4,16 +4,23 @@ import { Component } from '@angular/core';
     selector: 'app-header',
     template: `<nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="">
+      <a class="navbar-item" routerLink="">
        {{ title }} 
       </a>
-  
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+ <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
+    <div id="navbarExampleTransparentExample" class="navbar-menu">
+    <div class="navbar-start">
+    <a class="navbar-item" routerLink="property-binding">Property Binding</a>
+    <a class="navbar-item" routerLink="products">Products</a>
+    <a class="navbar-item" routerLink="aboutus">About Us</a>
+      
+          </div>
+          </div>
   </nav>
     `
     
